bbfab08b59dac8090139dcbe52a582c6285b1d02
Hugow1 <hugo.winder@gmail.com>
2022-10-18 13:54:47 +0200

    Config test

--- utils/config.ts
+++ utils/config.ts
@@ -1,5 +1,3 @@
 const dev = process.env.NEXT_PUBLIC_BASE_URL == "http://localhost:3000";
 
-export const server = dev
-  ? "http://localhost:3000"
-  : "https://vocal-bublanina-f99c73.netlify.app";
+export const server = dev ? "http://localhost:3000" : "http://localhost:3000";
